6d5fe37f9206f664fc383563077de657bcf49284
chimchimster <jorddyishere@gmail.com>
2023-01-10 17:59:17 +0500

    Added Templates and layout for them

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+from .models import Women
 
 # Create your views here.
+menu = ["About Site", "Add Article", "Remove Article", "Leave Comment"]
+def index(request): #link to class HttpRequest
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {
+        'title': 'Main Page',
+        'menu': menu,
+        'posts': posts,
+    })
+
+def about(request):
+    return render(request, 'women/about.html', {
+        'title': 'About Site',
+        'menu': menu,
+    })
+
+def categories(request, catid):
+    if request.GET:
+        print(request.GET)
+
+    return HttpResponse(f'<h1>Articles under the categories</h1><p>{catid}</p>')
+
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home', permanent=True)
+
+    return HttpResponse(f'<h1>Years Archive</h1><p>{year}</p>')
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Page doesn't found</h1>")
