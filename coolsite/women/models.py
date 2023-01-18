d7341c804f25f43432b7c584298df143ab08687d
chimchimster <jorddyishere@gmail.com>
2023-01-18 12:14:09 +0500

    added admin panel functionality

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -3,13 +3,13 @@ from django.urls import reverse
 
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name="Name")
+    content = models.TextField(blank=True, verbose_name="Content")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d', verbose_name="Picture")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Edited")
+    is_published = models.BooleanField(default=True, verbose_name="Published")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Category")
 
     def __str__(self):
         return self.title
@@ -17,11 +17,21 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Celebrity'
+        verbose_name_plural = 'Celebrities'
+        ordering = ['-time_create', 'title']
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Category")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['id']
