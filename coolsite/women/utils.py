401b5af8acc22c122707c55b2c33e60b7f149103
chimchimster <jorddyishere@gmail.com>
2023-01-20 17:14:02 +0500

    resolved merge problems

--- coolsite/women/utils.py
+++ coolsite/women/utils.py
@@ -11,6 +11,7 @@ menu = [{'title': 'About', 'url_name': 'about'},
 
 
 class DataMixin:
+    paginate_by = 3
     def get_user_context(self, **kwargs):
         context = kwargs
         cats = Category.objects.annotate(Count('women'))
