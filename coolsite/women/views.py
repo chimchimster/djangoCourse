dbbdfda84a910903156f0927f5859fde0ec9b84c
chimchimster <jorddyishere@gmail.com>
2023-01-22 13:31:05 +0500

    added captcha

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -6,9 +6,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import FormView
 
 from .models import Women, Category
-from .forms import AddPostForm, RegisterUserForm, LoginUserForm
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm, ContactForm
 from .utils import DataMixin
 
 
@@ -52,8 +53,19 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
-def contact(request):
-    return HttpResponse('Contacts')
+class ContactFromView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'women/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Contact')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("<h1>Page doesn't found</h1>")
